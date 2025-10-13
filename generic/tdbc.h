d67f1bcd06eaf37f8aab2b0b6a3eecf552e6527b
jan.nijtmans <jan.nijtmans>
2025-10-13 13:12:04 +0000

    Version => 1.1.13 (in preparation for next release)

--- generic/tdbc.h
+++ generic/tdbc.h
@@ -60,7 +60,7 @@ DLLIMPORT int		Tdbc_Init(Tcl_Interp* interp);
  */
 
 #define	TDBC_VERSION	"1.1"
-#define TDBC_PATCHLEVEL "1.1.12"
+#define TDBC_PATCHLEVEL "1.1.13"
 
 /*
  * Include the Stubs declarations for the public API, generated from
